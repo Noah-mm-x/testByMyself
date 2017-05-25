ec05e6c105142d8c5acb502aee5110d50dc167dd
孟凡旭 <mengfanxu@qimai.net>
2017-05-25 14:03:40 +0800

    20170525

--- node/读取文件/main.js
+++ node/读取文件/main.js
@@ -1,8 +1,27 @@
-/**
- * Created by mengfanxu on 17/2/13.
- */
-var fs=require('fs');
-fs.readFile('index.txt',function (err,data) {
-   if(err) console.log(err);
-   console.log(data.toString());
+'use strict';
+var fs = require('fs');
+var request = require('request-promise');
+var writeStream = fs.createWriteStream('index.json');
+// 获取数据条数
+var num = 100
+new Promise((resolve, reject) => {
+	var arr = [];
+	var url;
+	for(let i=1;i<num;i++){
+		url = 'http://mnks.jxedt.com/get_question?r=0.14942907941122408&index='+i;
+		request(url).then((data)=>{
+			arr.push(data);
+		}).then((result)=>{
+		    if(i==(num-1)) resolve(arr);
+		})
+	}
+}).then((result)=>{
+	writeStream.write(result.toString(),'UTF8');
+	writeStream.on('finish',()=>{
+		console.log('写入完成');
+	});
+	writeStream.on('error',()=>{
+		console.log('写入错误');
+	});
+	writeStream.end();
 });
